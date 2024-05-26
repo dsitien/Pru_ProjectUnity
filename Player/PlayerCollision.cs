1855ba37147a98805cc340c1e4300e84464f1f68
Si Tien <duongsitien123@gmail.com>
2024-05-26 21:46:56 +0700

    HP

---
+++ Player/PlayerCollision.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+	private Color originalColor;
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		originalColor = spriteRenderer.color;
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Enemy"))
+		{
+			PlayerHeart.health--;
+			if (PlayerHeart.health <= 0)
+			{
+
+			}else
+			{
+				StartCoroutine(GetHurt());
+			}
+		}
+	}
+
+	IEnumerator GetHurt()
+	{
+		Physics2D.IgnoreLayerCollision(6,7,true);
+
+		for (int i = 0; i < 5; i++) // Nhấp nháy 5 lần
+		{
+			SetTransparency(0.5f); // Đặt độ trong suốt xuống 50%
+			yield return new WaitForSeconds(0.2f);
+			SetTransparency(1f); // Đặt lại độ trong suốt về 100%
+			yield return new WaitForSeconds(0.2f);
+		}
+
+
+		Physics2D.IgnoreLayerCollision(6, 7,false);
+	}
+	void SetTransparency(float alpha)
+	{
+		Color color = spriteRenderer.color;
+		color.a = alpha;
+		spriteRenderer.color = color;
+	}
+}
